2f37d4e48043304a43ef20fe652a56036eb36219
garyhtg <garyhtg26@gmail.com>
2023-09-02 01:50:46 +0700

    detail kost 80%

--- resources/views/components/detail-kost.blade.php
+++ resources/views/components/detail-kost.blade.php
@@ -2,7 +2,7 @@
 
     <section id="detailKost">
         <div class="container">
-            <nav style="--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
+            <nav style="font-size: 0.8rem;--bs-breadcrumb-divider: url(&#34;data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='8' height='8'%3E%3Cpath d='M2.5 0L1 1.5 3.5 4 1 6.5 2.5 8l4-4-4-4z' fill='%236c757d'/%3E%3C/svg%3E&#34;);" aria-label="breadcrumb">
                 <ol class="breadcrumb">
                   <li class="breadcrumb-item"><a href="#">Home</a></li>
                   <li class="breadcrumb-item"><a href="#">Search</a></li>
@@ -11,16 +11,84 @@
                   <li class="breadcrumb-item active" aria-current="page">Kost Andromeda</li>
                 </ol>
               </nav>
-              <div class="row">
-                    <div class="col-md-6 col-sm-12"> 
-                        <span class="badge text-bg-success">Verified</span>
-                        <h1>Kost Andromeda Menteng Residence</h1>
-                        <p><span class="me-2" style="color:grey">Type :</span><b> A</b></p>
-                        <p>Jl. Menteng Raya No.21, RT.1/RW.10, Kb. Sirih, Kec. Menteng, Jakarta Pusat</p>
-                        <div class="d-flex mt-4" style="gap:10px">
-                            <div class="btn btn-secondary label-all">All</div>
-                            <div class="btn btn-warning label-star"><img src="img/star.svg" class="me-2">4.5</div>
-                            <div class="card card-location px-2"><a class="px-2"><img class="me-2" src="img/location-dot.svg">See on Maps</a></div>
+              <div class="row mt-4">
+                    <div class="col-md-6 col-sm-12">
+                        <div class="">
+                            <div class="kost-overview">
+                                <span class="badge text-bg-success">Verified</span>
+                                <h1>Kost Andromeda Menteng Residence</h1>
+                                <p><span class="me-2" style="color:grey">Type :</span><b> A</b></p>
+                                <p>Jl. Menteng Raya No.21, RT.1/RW.10, Kb. Sirih, Kec. Menteng, Jakarta Pusat</p>
+                                <div class="d-flex mt-4" style="gap:10px">
+                                    <div class="btn btn-secondary label-all">All</div>
+                                    <div class="btn btn-warning label-star"><img src="img/star.svg" class="me-2">4.5</div>
+                                    <div class="card card-location px-2"><a class="px-2"><img class="me-2" src="img/location-dot.svg">See on Maps</a></div>
+                                </div>
+                                <div class="mt-5">
+                                    <img style="position: absolute; left:35px" src="img/pricetag.svg" height="35rem">
+                                    <h3>Rp 1.000.000 <span class="text-grey-bold">/ Month</span></h3>
+                                </div>
+                            </div>
+                            <div class="kost-Facilities mt-5">
+                                <h3 class="subtitle-kost">Facilities</h3>
+                                <ul class="row mt-5">
+                                    <li class="col-6">
+                                      AC
+                                    </li>
+                                    <li class="col-6">
+                                      Kasur
+                                    </li>
+                                    <li class="col-6">
+                                      Kursi
+                                    </li>
+                                    <li class="col-6">
+                                      Meja
+                                    </li>
+                                    <li class="col-6">
+                                      Lemari
+                                    </li>
+                                    <li class="col-6">
+                                      Jendela
+                                    </li>
+                                    <li class="col-6">
+                                      Kasur
+                                    </li>
+                                    <li class="col-6">
+                                      Jemuran
+                                    </li>
+
+                                  </ul>
+                            </div>
+                            <div class="kost-Facilities mt-5">
+                                <h3 class="subtitle-kost">Others</h3>
+                                <ul class="row mt-5">
+                                    <li class="col-6">
+                                      K. Mandi Dalam
+                                    </li>
+                                    <li class="col-6">
+                                      Sudah Termasuk Listrik
+                                    </li>
+                                    <li class="col-6">
+                                      Boleh Pasutri
+                                    </li>
+                                    <li class="col-6">
+                                      Meja
+                                    </li>
+                                    <li class="col-6">
+                                      Lemari
+                                    </li>
+                                    <li class="col-6">
+                                      Jendela
+                                    </li>
+                                    <li class="col-6">
+                                      Kasur
+                                    </li>
+                                    <li class="col-6">
+                                      Jemuran
+                                    </li>
+
+                                  </ul>
+                            </div>
                         </div>
                     </div>
                     <div class="col-md-6 col-sm-12">
